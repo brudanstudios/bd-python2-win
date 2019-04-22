156d4002aa7333b5a88c47268e96219a3bf0ff47
geosam <geosam@oneanimation.com>
2019-04-22 11:20:52 +0800

    first commit

---
+++ pythonw.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enableextensions EnableDelayedExpansion
+goto :main
+
+
+:main
+setlocal
+
+	rem -- get absolute location of this folder (note- always ends with a \)
+	set CURRENT_DIR=%~dp0
+
+	rem -- remove the trailing slash otherwise it causes all sorts of problems later!
+	IF %CURRENT_DIR:~-1%==\ SET CURRENT_DIR=%CURRENT_DIR:~0,-1%
+
+	%CURRENT_DIR%\bin\pythonw %*
+
+endlocal
+goto :eof
